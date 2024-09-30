3151b8cdcdd2b26c3cffdc969efe983d2b284e78
Kim Sunghwan <kgmyh@naver.com>
2024-09-30 14:39:49 +0900

    07

---
+++ 02_Python/greeting_module.py
@@ -0,0 +1,23 @@
+# 인사말 관련 모듈.
+__version__ = 1.0
+
+def print_greeting(name):
+    print(f"{name}님 안녕하세요.")
+
+def print_welcome(name):
+    print(f"{name}님 환영합니다.")
+
+def get_greeting(name):
+    return f"{name}님 안녕하세요."
+
+class Hello:
+
+    def __init__(self, name):
+        self.name = name
+
+    def kor_greet(self):
+        return f"{name}님 안녕하세요."
+
+    def eng_greet(self):
+         return f"Hello {name}~!"
+
