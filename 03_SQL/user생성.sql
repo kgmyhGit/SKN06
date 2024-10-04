562b01a229b0cd420a63ccb27a6fd068f7a2a372
Kim Sunghwan <kgmyh@naver.com>
2024-10-04 15:50:00 +0900

    DDL 추가

--- 03_SQL/user생성.sql
+++ 03_SQL/user생성.sql
@@ -22,3 +22,40 @@ select host, user from mysql.user;
 -- 계정 삭제
 drop user scott@'%';
 drop user scott@localhost;
+
+-- db 조회
+show databases;
+
+-- database를 생성
+create database test;
+
+use test;
+
+drop table member;
+create table member(
+  num int unsigned auto_increment primary key,
+  id varchar(20) not null,
+  password varchar(20) not null,
+  name varchar(50) not null,
+  address varchar(100), 
+  join_date datetime not null,
+  email varchar(100) unique,
+  gender char(1) not null check(gender in ('M', 'F'))
+);
+-- insert into member (id, password, name, join_date, gender) 
+-- values ('id', 'aaaaaa', '이순신', '2020-01-02 10:10:20', '남');
+
+insert into member (id, password, name, join_date, gender) 
+values ('id-20', 'aaaaaa', '이순신', '2020-01-02 10:10:20', 'M');
+
+insert into member values (100, 'id-10', 'abc111', '홍길동', '서울',
+'2010-10-20 16:22:32', 'abc@a.com', 'M');
+
+select * from member;
+
+
+-- table 확인
+show tables;
+-- table의 컬럼(속성)들 확인. desc 테이블이름
+desc member;
+
