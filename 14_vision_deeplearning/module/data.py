527d3a4e77a0246baf2de108547487c9678684e0
Kim Sunghwan <kgmyh@naver.com>
2024-12-19 14:49:52 +0900

    up

--- 14_vision_deeplearning/module/data.py
+++ 14_vision_deeplearning/module/data.py
@@ -57,23 +57,3 @@ def load_cifar10_dataset(root_path, batch_size, is_train=True, transform=None):
     dataset = datasets.CIFAR10(root=root_path, train=is_train, download=True, transform=transform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=is_train, drop_last=is_train)
     return dataloader
-
-def load_cifar10_dataset(root_path, batch_size, is_train=True, transform=None):
-    """
-    cifar10 dataset dataloader 제공 함수
-    [parameter]
-        root_path: str|Path - 데이터파일 저장 디렉토리
-        batch_size: int
-        is_train: bool = True - True: Train dataset, False - Test dataset
-    [return]
-        DataLoader
-    """
-    if transform == None:
-        
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-        ])
-    dataset = datasets.CIFAR10(root=root_path, train=is_train, download=True, transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=is_train, drop_last=is_train)
-    return dataloader
